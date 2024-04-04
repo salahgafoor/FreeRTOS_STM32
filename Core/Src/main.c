48ed72acc6f157e362f3f56be7b1f7146d73d9c4
salahgafoor <salahgafoor@gmail.com>
2024-04-04 09:24:09 +0530

    project renamed from FreeRTOS_Projects to 001Tasks; added 2 tasks

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "FreeRTOS.h"
+#include "task.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -48,7 +49,8 @@
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
-
+static void task1_handler(void *parameters);
+static void task2_handler(void *parameters);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -63,7 +65,9 @@ static void MX_GPIO_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	TaskHandle_t task1_handle;
+	TaskHandle_t task2_handle;
+	BaseType_t status;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -85,7 +89,11 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
+  status = xTaskCreate(task1_handler, "Task-1", 200, "Hello from Task 1", 2, &task1_handle);
+  configASSERT(status == pdPASS);
 
+  status = xTaskCreate(task2_handler, "Task-2", 200, "Hello from Task 2", 2, &task2_handle);
+  configASSERT(status == pdPASS);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -181,7 +189,14 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+static void task1_handler(void *parameters)
+{
 
+}
+static void task2_handler(void *parameters)
+{
+
+}
 /* USER CODE END 4 */
 
 /**
